d60a20669f4d1daacb1f07674aa879110ba40d48
kosovito <kosovito@gmail.com>
2023-01-04 21:50:48 +0100

    04012023 - 2

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+  id;
+  nombre;
+  apellido;
+  createAt;
+  email;
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Álvaro', apellido: 'García', email: 'alvaro@falso.com', createAt: '2023-01-04' },
+  { id: 2, nombre: 'Jos', apellido: 'es', email: 'ese@es.com', createAt: '2017-12-13' },
+  { id: 3, nombre: 'Sol', apellido: 'esmi', email: 'backe@es.com', createAt: '2017-12-15' },
+  { id: 4, nombre: 'front', apellido: 'back', email: 'estu@es.com', createAt: '2017-11-11' },
+  { id: 5, nombre: 'dian', apellido: 'te', email: 'morf@es.com', createAt: '2017-12-11' },
+  { id: 6, nombre: 'cliens', apellido: 'wiof', email: 'wiofclients@es.com', createAt: '2011-12-11' },
+  { id: 7, nombre: 'baka', apellido: 'sd', email: 'noprl21@es.com', createAt: '2012-12-11' },
+  { id: 8, nombre: 'reami', apellido: 'fpon', email: 'swar@es.com', createAt: '2015-12-11' },
+];
